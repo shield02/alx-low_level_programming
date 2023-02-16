6747dc49ac6d8cc27d4282d2bdd8fda21d3bf33c
Otuekong <myshield02@gmail.com>
2023-02-16 11:51:26 -0800

    added description for main and new line code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Main - Main block
+ * main - main block
  * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
