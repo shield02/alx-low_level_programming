2e591c7fe79f8fcafa1fa0a272e75f8197984c66
Otuekong <myshield02@gmail.com>
2023-02-17 05:20:52 -0800

    code to print small letters than capittal letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: This prints an alphabet in lowercase, then uppercase
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
+
