6a01e3a324b03ab8f82905ffa2e5dbe070af5281
Otuekong <myshield02@gmail.com>
2023-02-21 13:39:57 -0800

    code to add two integer values and return result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two integer values
+ * @var1: int variable
+ * @var2: int variable
+ * Return: 0
+ */
+int add(int var1, int var2)
+{
+	return (var1 + var2);
+}
