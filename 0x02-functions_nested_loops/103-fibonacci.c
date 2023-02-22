5ae0dff1963b42c66a51aba3dc9659eaecd61351
Otuekong <myshield02@gmail.com>
2023-02-21 18:49:52 -0800

    code to add fibonacci numbers from 1,2 to less than 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry block
+ * Description: create a fibonacci sequence starting from 1, 2 but
+ * no more than 4,000,000
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0, b = 1, next = 0;
+	int sum = 0;
+
+	while (next < 4000000)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		if (next % 2 == 0)
+			sum += next;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
