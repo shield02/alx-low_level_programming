d60968757bcd758fc874d45d15308dbaa35a1444
Otuekong <myshield02@gmail.com>
2023-02-22 23:39:40 -0800

    code to check for uppercase letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper(int c) - checks for uppercase characters
+ * @c: int variable to be checked
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= "A" && c <= "Z")
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
