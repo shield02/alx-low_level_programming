78d455ea0d94b299cedfa20af9330f3835c152a9
Otuekong <myshield02@gmail.com>
2023-02-22 23:49:15 -0800

    code to check for digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int variable to be checked
+ * Return: 1 for true or 0 for false
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && <= 57);
+}
