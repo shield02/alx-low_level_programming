fef2587e4d3d6b2615497521a90b4404cb726469
Otuekong <myshield02@gmail.com>
2023-02-27 09:18:13 -0800

    Code modification

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,14 +12,14 @@
 void print_rev(char *s)
 {
 	int i = 0;
-	
+
 	while (s[i])
 		i++;
 
-	for (i = 0; s[i] != '\0'; --i)
+	for (i = 0; s[i] >= '\0'; --i)
 	{
 		_putchar(s[i]);
 	}
-	
+
 	_putchar('\n');
 }
