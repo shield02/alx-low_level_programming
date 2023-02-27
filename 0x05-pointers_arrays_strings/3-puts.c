ebb4340ed79d35da5536e5962563f087e83e0f32
Otuekong <myshield02@gmail.com>
2023-02-27 08:31:06 -0800

    code to print to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include  "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ * @str: a char var to be printed to stdout
+ *
+ * Description: this function prints a string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int = i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
