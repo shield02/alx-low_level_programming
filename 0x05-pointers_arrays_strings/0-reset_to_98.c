bd3628af62d2f4e60cfb3e471284442bff3a6dd2
Otuekong <myshield02@gmail.com>
2023-02-27 00:47:43 -0800

    added proper description

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,10 @@
 
 /**
  * reset_to_98 - update the value of a pointer to 98
- * @*n: pointer
+ * @n: pointer
+ *
+ * Description: This function takes a pointer to an int
+ * as parameter and updates the value to 98
  * Return: void
  */
 
