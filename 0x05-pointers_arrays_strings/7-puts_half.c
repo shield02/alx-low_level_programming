ce52a64248316be357663a65a1bda1b7f4cd3f94
Otuekong <myshield02@gmail.com>
2023-02-27 11:52:13 -0800

    code modification

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -22,7 +22,7 @@ void puts_half(char *str)
 		for (j = i / 2; str[j] != '\0'; j++)
 			_putchar(str[j]);
 	}
-	else if
+	else if (i % 2)
 	{
 		for (k = (i - 1) / 2; k < i - 1; k++)
 			_putchar(str[k + 1]);
