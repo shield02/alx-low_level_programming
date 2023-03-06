ba80124d43db9d767b2e94bc57b9bc8c21fc5380
Otuekong <myshield02@gmail.com>
2023-03-06 05:28:12 -0800

    code to copy memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copies memory area
+ * @dest: char variable
+ * @src: char variable
+ * @n: unsigned int variable
+ *
+ * Description: This function copies memory area
+ * Return: *dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
