a69e4478ed77b6afee9d430348d862ea5a2493b7
Otuekong <myshield02@gmail.com>
2023-03-10 11:08:52 -0800

    code to check isupper

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if the value is upper.
+ *
+ * @c: int variable
+ *
+ * Return: Always 0.
+ */
+
+
+int _isupper(int c)
+{
+
+	return (c >= 65 && c <= 90);
+}
