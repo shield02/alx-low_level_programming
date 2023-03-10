48dbe43ed863c87915972d5082d65ead07ee17c3
Otuekong <myshield02@gmail.com>
2023-03-10 11:48:00 -0800

    code for isdigit

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a value is a digit
+ * @c: is an integer param
+ *
+ * Description: This function checks if it is a digit
+ * Return: Always 0.
+ */
+
+
+int _isdigit(int c)
+{
+
+	return (c >= 48 && c <= 57);
+}
