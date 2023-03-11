368c9564c78494a93028d04f31657c4563d23520
Otuekong <myshield02@gmail.com>
2023-03-11 05:37:44 -0800

    compares two strings

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,1 +1,25 @@
-int _strcmp(char *s1, char *s2) {}
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: char variable
+ * @s2: char variable
+ *
+ * Description: This function takes two strings and compares them
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+}
+
