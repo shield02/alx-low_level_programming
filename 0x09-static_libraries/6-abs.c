d29ea3d203cc7bd218201cc9c4b25d582efd5951
Otuekong <myshield02@gmail.com>
2023-03-11 06:02:10 -0800

    get the absolute value of an int

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,1 +1,17 @@
-int _abs(int n) {}
+#include "main.h"
+
+/**
+ * _abs - get the absolute value of an int
+ * @n: int variable
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
+
