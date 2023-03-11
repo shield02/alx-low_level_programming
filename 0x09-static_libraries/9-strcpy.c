ae10558f674989ed5258a54ca5792c0c5a4c642d
Otuekong <myshield02@gmail.com>
2023-03-11 05:27:30 -0800

    script copies a string pointed by a file

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,1 +1,27 @@
-char *_strcpy(char *dest, char *src) {}
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: char var, destination path
+ * @src: char var, source path
+ *
+ * Description: this function copies the string pointed
+ * to by src, including the terminating null byte (\0),
+ * to the buffer pointer to by dest
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int j;
+
+	while (src[i] != '\0')
+		i++;
+
+	for (j = 0; j <= i; j++)
+		dest[j] = src[j];
+
+	return (dest);
+}
+
