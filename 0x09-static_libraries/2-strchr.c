70b404987ff5b21db3904bcaa03bc5929fcc50bd
Otuekong <myshield02@gmail.com>
2023-03-11 05:43:17 -0800

    locates a char in a string

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,1 +1,28 @@
-char *_strchr(char *s, char c) {}
+#include "main.h"
+
+/**
+ * *_strchr - locates a char in a string
+ * @s: char variable
+ * @c: character to be located in s
+ *
+ * Description: This function locates a chararacter in a string
+ * Return: *S
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+return (0);
+}
+
