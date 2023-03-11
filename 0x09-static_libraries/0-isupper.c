2e94fdf1b99202922db8cf2b83367812496c709c
Otuekong <myshield02@gmail.com>
2023-03-11 02:57:45 -0800

    added content to prototype

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,1 +1,15 @@
-int _isupper(int c) {}
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase characters
+ * @c: int variable to be checked
+ * Return: 1 if uppercase or 0 if not
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
+
