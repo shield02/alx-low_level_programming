a9b41d4ac9f5597df6c71ecf09c5a163daee5528
Otuekong <myshield02@gmail.com>
2023-03-11 05:58:23 -0800

    breaks a string

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,1 +1,28 @@
-char *_strpbrk(char *s, char *accept) {}
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - breaks a string
+ * @s: pointer to char
+ * @accept: pointer to char
+ * Return: NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
+
