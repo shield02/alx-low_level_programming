ff08317acb299b1ea64c457475097e263b9436aa
Otuekong <myshield02@gmail.com>
2023-03-11 05:32:26 -0800

    checks for a digit (0 through 9)

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,1 +1,13 @@
-int _isdigit(int c) {}
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int variable to be checked
+ * Return: 1 for true or 0 for false
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
+
