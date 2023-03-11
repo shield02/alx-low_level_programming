818d467874e4e10a6f133d10f43a8b419d7c5baf
Otuekong <myshield02@gmail.com>
2023-03-11 05:42:06 -0800

    copies memomery area

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,1 +1,23 @@
-char *_memcpy(char *dest, char *src, unsigned int n) {}
+#include "main.h"
+
+/**
+ * *_memcpy - copies memory area
+ * @dest: char variable
+ * @src: char variable
+ * @n: unsigned int variable
+ *
+ * Description: This function copies memory area
+ * Return: *dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+
