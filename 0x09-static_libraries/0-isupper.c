b357b8962e92eae4404b7729adb4aa4a4ae783d6
Otuekong <myshield02@gmail.com>
2023-03-11 03:30:28 -0800

    code modified

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -3,12 +3,13 @@
 /**
  * _isupper - checks for uppercase characters
  * @c: int variable to be checked
+ *
+ * Description: This function checks if a letter is uppercase
  * Return: 1 if uppercase or 0 if not
  */
+
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
+	return (c >= 65 && c <= 90);
 }
+
