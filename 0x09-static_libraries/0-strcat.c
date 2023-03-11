4c7316a8eea94333d5dcdfe59bb937d75fa15dad
Otuekong <myshield02@gmail.com>
2023-03-11 05:34:44 -0800

    function that concats two strings

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,2 +1,29 @@
-char *_strcat(char *dest, char *src) {}
+#include "main.h"
+
+/**
+ * *_strcat - function that concatenates two strings
+ * @dest: char variable, string will be attaced to it
+ * @src: char variable, string that will be appended to dest
+ *
+ * Description: This function appends the src string to the
+ * dest string, overwriting the terminating null byte (\0)
+ * at the end of dest, and then adds a terminating null byte
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a = -1, i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	do {
+		a++;
+		dest[i] = src[a];
+		i++;
+	} while (src[a] != '\0');
+
+	return (dest);
+}
 
