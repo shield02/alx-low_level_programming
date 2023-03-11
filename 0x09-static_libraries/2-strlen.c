2a857bb00a663163016915eda4ae0645c3d1699d
Otuekong <myshield02@gmail.com>
2023-03-11 05:28:43 -0800

    code gets the length of a string

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,1 +1,20 @@
-int _strlen(char *s) {}
+#include "main.h"
+
+/**
+ * _strlen - gets the length of a string
+ * @s: char var, the string
+ *
+ * Description: this function returns the length of a string
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+		continue;
+
+	return (i);
+}
+
