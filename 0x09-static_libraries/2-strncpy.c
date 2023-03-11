82c441047319930e0f9f2315286f5a1788735fb2
Otuekong <myshield02@gmail.com>
2023-03-11 05:36:28 -0800

    code that cpoies a string

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,1 +1,26 @@
-char *_strncpy(char *dest, char *src, int n) {}
+#include "main.h"
+
+/**
+ * *_strncpy - that copies a string
+ * @dest: char variable
+ * @src: char variable
+ * @n: int variable
+ *
+ * Description: This function copies a string.
+ * It works exactly like strncpy
+ * Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
+
