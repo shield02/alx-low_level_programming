a6515dd96234bb56c84e3ff8d095d17e3adb1ea8
Otuekong <myshield02@gmail.com>
2023-03-11 05:47:27 -0800

    checks if the string is lower

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,1 +1,17 @@
-int _islower(int c) {}
+#include "main.h"
+
+/**
+ * _islower - checks if a string is lower
+ * @c: int variable
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
+
