647e032dd3ce03240a46d1303ae6c6af97f1ce6f
Otuekong <myshield02@gmail.com>
2023-03-10 17:57:51 -0800

    code that copies a memory

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memory
+ * @dest: pointer to char params
+ * @src: pointer to char params
+ * @n: size
+ *
+ * Description: This function copies memory
+ * Return: *dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
