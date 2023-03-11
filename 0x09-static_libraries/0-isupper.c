824d2a8ec05333c47f8ac33b6d9581860816d30e
Otuekong <myshield02@gmail.com>
2023-03-10 23:09:11 -0800

    code prototype

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,18 +1,1 @@
-#!/bin/bash
-
-#include "main.h"
-
-/**
- * _isupper - checks if the value is upper.
- * @c: int variable
- *
- * Description: This function checks if the value is upper.
- * Return: 0.
- */
-
-
-int _isupper(int c)
-{
-
-	return (c >= 65 && c <= 90);
-}
+int _isupper(int c) {}
