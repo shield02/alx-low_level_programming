defde3fb1228a8e2a6728de47bdb35a056abcd65
Otuekong <myshield02@gmail.com>
2023-03-11 03:53:16 -0800

    line rearrange

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -2,10 +2,10 @@
 #define FILE_MAIN
 
 int _putchar(char c);
+int _isupper(int c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
-int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
