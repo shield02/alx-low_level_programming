4da2682c5750c701dccfe5e6a80884d43ffecddb
Otuekong <myshield02@gmail.com>
2023-03-11 05:25:56 -0800

    code to print a string to stdout

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,1 +1,22 @@
-void _puts(char *s) {}
+#include  "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ * @str: a char var to be printed to stdout
+ *
+ * Description: this function prints a string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
+
