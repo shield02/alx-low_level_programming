4e670072dd3cf8970531c29e3ca76a91d620b6cf
Otuekong <myshield02@gmail.com>
2023-03-11 05:55:09 -0800

    code to check if it is alpha

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,1 +1,22 @@
-int _isalpha(int c) {}
+#include "main.h"
+
+/**
+ * _isalpha - code to check if it is alpha
+ * @c: int arguement
+ * Return: Always 0
+ */
+
+int _isalpha(int c)
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
+
