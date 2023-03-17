e90061c35f9d15f529ef60a73cc9f5914c914899
Otuekong <myshield02@gmail.com>
2023-03-16 21:45:16 -0700

    reallocates memory block

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,12 +25,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return (0);
+		return (NULL);
 	}
 	relloc = malloc(new_size);
 	if (relloc == NULL)
-		return (0);
-	for (i = 0; i < old_size || i < new_size; i++)
+		return (NULL);
+	for (i = 0; i < old_size && i < new_size; i++)
 	{
 		*(relloc + i) = clone[i];
 	}
