2188da48f709e70a4dde9b165cbe7b4372f6525e
Otuekong <myshield02@gmail.com>
2023-03-22 21:25:42 -0700

    Added description to each function and return line to file

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -5,6 +5,8 @@
  * op_add - finds sum of a and b
  * @a: int variable
  * @b: int variable
+ *
+ * Description: This function finds the sum
  * Return: Sum of a and b
  */
 int op_add(int a, int b)
@@ -16,6 +18,8 @@ int op_add(int a, int b)
  * op_sub - finds difference of a and b
  * @a: int variable
  * @b: int variable
+ *
+ * Description: This function finds the difference
  * Return: the difference
  */
 int op_sub(int a, int b)
@@ -27,6 +31,8 @@ int op_sub(int a, int b)
  * op_mul - finds product of a and b
  * @a: int variable
  * @b: int variable
+ *
+ * Description: This function finds the product
  * Return: the product
  */
 int op_mul(int a, int b)
@@ -38,6 +44,8 @@ int op_mul(int a, int b)
  * op_div - finds quotient of a and b
  * @a: int variable
  * @b: int variable
+ *
+ * Description: This function finds the quotient
  * Return: the quotient
  */
 int op_div(int a, int b)
@@ -54,6 +62,8 @@ int op_div(int a, int b)
  * op_mod - finds the remainder of a and b
  * @a: int variable
  * @b: int variable
+ *
+ * Description: This function finds the modulo
  * Return: the remainder
  */
 int op_mod(int a, int b)
