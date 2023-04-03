8b0515f0d9b0cd3d7d15f920281bf635e86b6648
Otuekong <myshield02@gmail.com>
2023-04-03 12:33:34 -0700

    Corrected functions for both listint_len and print_listint

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -12,14 +12,14 @@
  * Description: singly linked list node
  *
  */
-typedof struct listint_s
+typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const listint *h);
-size_t listint_len(const listint *h);
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 
 #endif
