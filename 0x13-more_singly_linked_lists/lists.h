2005e49a7d4a6bec876a684e8e64be0420cb15ff
Otuekong <myshield02@gmail.com>
2023-04-02 23:30:43 -0700

    Initial commit for this project header file with just the structure code

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node
+ *
+ */
+typedof struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+
+#endif
+
