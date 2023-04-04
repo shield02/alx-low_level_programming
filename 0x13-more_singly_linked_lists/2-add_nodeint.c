1585fb27e911fc2e46fd036f015523c8686f2a2f
Otuekong <myshield02@gmail.com>
2023-04-04 08:55:10 -0700

    Added a description

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,11 +1,24 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * add_nodeint - Add a new node
+ * @head: struct type arg
+ * @n: int arg
+ *
+ * Description: This function add a new node at the beginning of a string list
+ */
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *p;
 
 	p = malloc(sizeof(listint_t));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
 	p->n = n;
 	p->next = *head;
 	*head = p;
