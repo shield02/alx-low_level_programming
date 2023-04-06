2074d4919c401bc1f335c08d9f09cf2e1592a2f5
Otuekong <myshield02@gmail.com>
2023-04-05 20:45:17 -0700

    just the function structure yet

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_listint_safe - frees a list
+ * @h: struct type arg
+ *
+ * Description: This function frees a linked list
+ * Return: the size of the list
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *tmp;
+	int d;
+	size_t len = 0;
+
+	return (len);
+}
+
