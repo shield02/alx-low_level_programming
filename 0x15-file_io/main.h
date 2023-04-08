8c1ee5cecca6eae1f79a5a5d675ffd41e9b2b53f
Otuekong <myshield02@gmail.com>
2023-04-08 11:14:19 -0700

    added some header files

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
 int _putchar(char c);
 
 ssize_t read_textfile(const char *filename, size_t letters);
