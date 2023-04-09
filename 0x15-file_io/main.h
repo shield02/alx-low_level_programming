db51860aec8bd07c7fbddad2c284a536eca83baf
Otuekong <myshield02@gmail.com>
2023-04-09 02:57:44 -0700

    Added arg_error, read_error, write_error, close_error lines

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,10 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+void arg_error(void);
+void read_error(char *file);
+void write_error(char *file, int handle);
+void close_error(int handle);
+
 
 #endif
