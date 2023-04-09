f2d169c03e8fe72e8094802760b3d4482201a6c1
Otuekong <myshield02@gmail.com>
2023-04-09 11:59:23 -0700

    corrected spacing in printed output

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -39,7 +39,7 @@ void print_magic(unsigned char *e_ident)
 {
 	int n = 0;
 
-	printf("  Magic:    ");
+	printf("  Magic:   ");
 
 	do {
 		printf("%02x", e_ident[n]);
@@ -62,7 +62,7 @@ void print_magic(unsigned char *e_ident)
  */
 void print_class(unsigned char *e_ident)
 {
-	printf("  Class:                              ");
+	printf("  Class:                             ");
 
 	if (e_ident[EI_CLASS] == ELFCLASSNONE)
 		printf("none\n");
@@ -84,7 +84,7 @@ void print_class(unsigned char *e_ident)
  */
 void print_data(unsigned char *e_ident)
 {
-	printf("  Data:                               ");
+	printf("  Data:                              ");
 	if (e_ident[EI_DATA] == ELFDATANONE)
 		printf("none\n");
 	else if (e_ident[EI_DATA] == ELFDATA2LSB)
@@ -128,7 +128,7 @@ void print_version(unsigned char *e_ident)
  */
 void print_osabi(unsigned char *e_ident)
 {
-	printf("  OS/ABI:                             ");
+	printf("  OS/ABI:                            ");
 	if (e_ident[EI_OSABI] == ELFOSABI_NONE)
 		printf("UNIX - System V\n");
 	else if (e_ident[EI_OSABI] == ELFOSABI_HPUX)
@@ -163,7 +163,7 @@ void print_osabi(unsigned char *e_ident)
  */
 void print_abi(unsigned char *e_ident)
 {
-	printf("  ABI Version:                     %d\n",
+	printf("  ABI Version:                       %d\n",
 		e_ident[EI_ABIVERSION]);
 }
 
@@ -180,7 +180,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 {
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 		e_type >>= 8;
-	printf("  Type:                               ");
+	printf("  Type:                              ");
 	if (e_type == ET_NONE)
 		printf("NONE (None)\n");
 	else if (e_type == ET_REL)
@@ -206,7 +206,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
  */
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 {
-	printf("  Entry point address:                ");
+	printf("  Entry point address:               ");
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
 		e_entry = ((e_entry << 8) & 0xFF00FF00) |
