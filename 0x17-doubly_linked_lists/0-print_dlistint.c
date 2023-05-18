6dcb6449514e3089a6b71eb4c39cf9204b05109f
Otuekong <myshield02@gmail.com>
2023-05-18 19:18:47 +0100

    code to print all the elements of a doubly list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a doubly list
+ * @h: const list pointer
+ * 
+ * Description: This function prints all the elements of
+ * a doubly list
+ * Return: int - number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+        size_t nodes = 0;
+
+        while (h)
+        {
+                nodes++;
+                printf("%d\n", h->n);
+                h = h->next;
+        }
+
+        return (nodes);
+}
