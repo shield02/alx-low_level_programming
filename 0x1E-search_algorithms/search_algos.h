57b26a98ec3fd062dddb872af9a38b8c85c9b01a
shield <myshield02@gmail.com>
2023-09-13 11:37:19 +0100

    added prototype for 100-jump

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,9 +4,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif
 
